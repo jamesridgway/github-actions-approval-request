9e314e7ab541b25b5bf577d2db8d42c6585e5504
James Ridgway <james@jamesridgway.co.uk>
2022-06-05 13:23:40 +0100

    Get workflow to trigger id

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const github = require('@actions/github');
 
 try {
     const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+    // Get contextual information
     const repositoryFullName = github.context.payload.repository.full_name;
     const commitHash = github.context.sha;
-    console.log(`The event payload for ${repositoryFullName} @ commit ${commitHash}: ${payload}`);
+    const workflowIdToTrigger = core.getInput('trigger-workflow');
+    console.log(`Trugger workflow ${workflowIdToTrigger} for ${repositoryFullName} with commit hash ${commitHash}`);
 } catch (error) {
     core.setFailed(error.message);
 }
