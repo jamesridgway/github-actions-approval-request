47e7b22d22a7c6ce3a4d4ae88bc1c9ca9994b67e
James Ridgway <james@jamesridgway.co.uk>
2022-06-05 13:18:30 +0100

    Log commit hash

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    const repositoryFullName = github.context.payload.repositoryFullName;
+    const commitHash = github.context.sha;
+    console.log(`The event payload for ${repositoryFullName} @ commit ${commitHash}: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
