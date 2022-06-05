574012fb0048568b5febd442f148165ca598f776
James Ridgway <james@jamesridgway.co.uk>
2022-06-05 13:19:39 +0100

    Repository name fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 try {
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    const repositoryFullName = github.context.payload.repositoryFullName;
+    const repositoryFullName = github.context.payload.repository.full_name;
     const commitHash = github.context.sha;
     console.log(`The event payload for ${repositoryFullName} @ commit ${commitHash}: ${payload}`);
 } catch (error) {
