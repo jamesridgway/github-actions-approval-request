ac536c045594c08e3466dba6b257fb04266b2473
James Ridgway <james@jamesridgway.co.uk>
2022-06-11 11:10:49 +0100

    Webhook update

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ try {
     const repositoryFullName = github.context.payload.repository.full_name;
     const commitHash = github.context.sha;
     const workflowIdToTrigger = core.getInput('trigger-workflow');
-    console.log(`Trugger workflow ${workflowIdToTrigger} for ${repositoryFullName} with commit hash ${commitHash}`);
+    const webhookUrl = core.getInput('notification-webhook-url');
+
+    console.log(`Triggering workflow ${workflowIdToTrigger} for ${repositoryFullName} with commit hash ${commitHash}`);
+
+    const webhookPayload = {
+        text: 'Hello world!'
+    }
+    axios.post(webhookUrl, webhookPayload)
+        .then(response => console.log(`Webhook response: ${response}`))
+        .catch(error => console.error('There was an error ', error));
 } catch (error) {
     core.setFailed(error.message);
 }
