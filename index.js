c15b0224c401dfe2b62653b44683d94e3f704f35
James Ridgway <james@jamesridgway.co.uk>
2022-06-11 11:12:45 +0100

    Axios import fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 try {
     const payload = JSON.stringify(github.context.payload, undefined, 2);
